a8e657801dfb5292bf145297bc50c9b56150fce8
lsj <lsj@pinetreesys.co.kr>
2021-12-29 13:37:13 +0900

    화폐단위
    아직 화폐단위가 바뀌는부분이 미흡함

--- 바던계/js/test.js
+++ 바던계/js/test.js
@@ -1,7 +1,11 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function fromFunction() {
+    document.getElementById("from-currency-list").classList.toggle("show");
+}
+
+function toFunction() {
+    document.getElementById("to-currency-list").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
@@ -33,16 +37,49 @@ let currencyRatio = {
         KRW: 40,
         USD: 38.123,
         unit: "바트"
-    }
-}
+    },
+};
 
-let fromCurrency = 'USD';
+let fromCurrency = "USD";
+let toCurrency = "KRW";
+let fromUnit = "달러";
+let toUnit = "원";
 
 document
     .querySelectorAll("#from-currency-list a")
     .forEach(menu => menu.addEventListener("click", function () {
         document.getElementById("from-button").textContent = this.textContent;
         fromCurrency = this.textContent;
-        console.log("fromCurrency는", fromCurrency);
+        // console.log("fromCurrency는", fromCurrency);
+
+        document.getElementById("from-unit").textContent = this.textContent;
+        fromUnit = this.textContent;
+        convert()
     }));
+
+document
+    .querySelectorAll("#to-currency-list a")
+    .forEach(menu => menu.addEventListener("click", function () {
+        document.getElementById("to-button").textContent = this.textContent;
+        toCurrency = this.textContent;
+        // console.log("toCurrency는", fromCurrency);
+        convert()
+    }));
+
+function convert() {
+    // console.log("키업 이벤트");
+    let amount = document.getElementById("from-input").value;
+    // console.log("돈은"+ amount);
+    let convertedAmount = amount * currencyRatio[fromCurrency][toCurrency]
+    // console.log("환전 결과 : " + convertedAmount);
+    document.getElementById("to-input").value = convertedAmount
+}
+
+/--------------------------------------------------------------------/
+
+function convertTo() {
+    let amount = document.getElementById("to-input").value;
+    let convertedAmount = amount * currencyRatio[toCurrency][fromCurrency]
+    document.getElementById("from-input").value = convertedAmount
+}
 
