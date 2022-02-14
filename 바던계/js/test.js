34342983e3fb6149fff914e5b97972666698f723
lsj <lsj@pinetreesys.co.kr>
2022-02-14 16:18:58 +0900

    나이계산기 추가(문자열)
    문자열로 (ex. 0000년샹)으로 들어와도 나이를 계산할 수 있도록

--- 바던계/js/test.js
+++ 바던계/js/test.js
@@ -1,27 +1,40 @@
-let Resident_num = '921215-1885522'
 
-let BDY = Resident_num.substring(0,2);
-let BDM = Resident_num.substring(2,4);
-let BDD = Resident_num.substring(4,6);
-let R_num = Resident_num.substring(7);
+var msg = 'Resident_num'
 
-let BDFullYear 
-if(R_num.substring(0,1)=='1' || R_num.substring(0,1)=='2'){
-    BDFullYear = 1900 + Number(BDY);
-} else if(R_num.substring(0,1)=='3' || R_num.substring(0,1)=='4'){
-    BDFullYear = 2000 + Number(BDY);
+var Resident_num = '9212151885522'
+var sentence = "1992년생인데 가입하려고요"
+
+
+if (msg == 'Resident_num') {
+    var BDY = Resident_num.substring(0, 2);
+    var BDM = Resident_num.substring(2, 4);
+    var BDD = Resident_num.substring(4, 6);
+    var R_num = Resident_num.substring(6);
+
+    var BDFullYear
+    if (R_num.substring(0, 1) == '1' || R_num.substring(0, 1) == '2') {
+        BDFullYear = 1900 + Number(BDY);
+    } else if (R_num.substring(0, 1) == '3' || R_num.substring(0, 1) == '4') {
+        BDFullYear = 2000 + Number(BDY);
+    }
+
+
+} else if (msg == 'sentence') {
+    var BDM = 0;
+    var BDD = 0;
+    var sen_wording = /([0-9\,\.]+)년생/g;
+    var sen_num = sentence.match(sen_wording)[0]
+
+    var BDFullYear = /([0-9\,\.]+)/g;
+    if (sen_num.match(BDFullYear)) {
+        BDFullYear = Number(sen_num.match(BDFullYear)[0])
+    }
 }
 
-let today = new Date();
-let birthdate = new Date(BDFullYear,BDM,BDD);
 
-let age = today.getFullYear() - birthdate.getFullYear();
-let m = today.getMonth() - birthdate.getMonth();
-if (m < 0 || (m == 0 && today.getDate() < birthdate.getDate())){
-    age--;
-}
-
-
+var today = new Date();
+var birthdate = new Date(BDFullYear, BDM, BDD);
+var age = today.getFullYear() - birthdate.getFullYear();
 
 
 console.log(age);
